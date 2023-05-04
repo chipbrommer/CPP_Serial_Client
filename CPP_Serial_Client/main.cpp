da8d03f39b19b0205ad1871233f2a6b65e0465ae
Chip Brommer <chipbrommer@gmail.com>
2023-05-03 21:25:30 -0500

    testing.

--- CPP_Serial_Client/main.cpp
+++ CPP_Serial_Client/main.cpp
@@ -10,7 +10,10 @@ int main()
 	std::cout << Essentials::Communications::SerialVersion;
 
 	Essentials::Communications::Serial serial;
-	serial.Configure("COMM4", Essentials::Communications::BaudRate::BAUDRATE_115200, Essentials::Communications::ByteSize::EIGHT, Essentials::Communications::Parity::NONE);
+	serial.Configure("COMM4", 
+		Essentials::Communications::BaudRate::BAUDRATE_115200, 
+		Essentials::Communications::ByteSize::EIGHT, 
+		Essentials::Communications::Parity::NONE);
 	serial.SetDelimiter("&");
 
 	std::string c;
